56ff1d539197ef5d5fde368101a385114b6c0771
felixgato123 <felixprimoestrada@gmail.com>
2024-09-12 19:07:58 -0500

    Create calculadora.cpp
    calculadora 1

---
+++ calculadora.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Variables para los números y el operador
+    double num1, num2;
+    char operacion;
+
+    // Solicitar al usuario el primer número
+    cout << "Ingresa el primer número: ";
+    cin >> num1;
+
+    // Solicitar al usuario el operador
+   
